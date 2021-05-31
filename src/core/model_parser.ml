e22c2ce04f632bb72240890eb0bb4e853c1e92df
Benedikt Becker <benedikt.becker@inria.fr>
2021-06-01 00:22:32 +0200

    Clean model element names late
    Change-Id: I1294c2ff6467ff8df9cba366b12a004d125117cd

--- src/core/model_parser.ml
+++ src/core/model_parser.ml
@@ -970,8 +970,6 @@ let build_model_rec pm (elts: model_element list) : model_files =
       let attrs, me_value = !remove_field (attrs, me_value) in
       (* Transform value flattened by eval_match (one field record) back to records *)
       let attrs, me_value = read_one_fields ~attrs me_value in
-      let name = get_model_trace_string ~name ~attrs in
-      let name = List.hd (Strings.bounded_split '@' name 2) in
       let me_name = create_model_element_name name attrs Other in
       {me_name; me_value; me_location= t.t_loc; me_term= Some t} in
     match Mstr.find_opt me.me_name.men_name pm.queried_terms with
@@ -1039,9 +1037,14 @@ class clean = object (self)
   method model m =
     {m with model_files= map_filter_model_files self#element m.model_files}
   method element me =
+    let me =
+      let name = me.me_name.men_name in
+      let attrs = me.me_name.men_attrs in
+      let name = get_model_trace_string ~name ~attrs in
+      let name = List.hd (Strings.bounded_split '@' name 2) in
+      {me with me_name = {me.me_name with men_name = name}} in
     if me.me_name.men_kind = Error_message then
-      (* Keep unparsed values for error messages *)
-      Some me
+      Some me (* Keep unparsed values for error messages *)
     else
       Opt.bind (self#value me.me_value) @@ fun me_value ->
       Some {me with me_value}
@@ -1052,10 +1055,10 @@ class clean = object (self)
     | Array a       -> self#array a    | Record fs     -> self#record fs
     | Undefined     -> self#undefined  | Var v         -> self#var v
   method const c = match c with
-      String v      -> self#string v  | Integer v     -> self#integer v  |
-      Decimal v     -> self#decimal v | Fraction v    -> self#fraction v |
-      Float v       -> self#float v   | Boolean v     -> self#boolean v  |
-      Bitvector v   -> self#bitvector v
+    | String v      -> self#string v  | Integer v     -> self#integer v
+    | Decimal v     -> self#decimal v | Fraction v    -> self#fraction v
+    | Float v       -> self#float v   | Boolean v     -> self#boolean v
+    | Bitvector v   -> self#bitvector v
   method string v = Some (Const (String v))
   method integer v = Some (Const (Integer v))
   method decimal v = Some (Const (Decimal v))
